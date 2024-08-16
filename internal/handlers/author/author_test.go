0933655c8fb8c30bbcecb6c2e91d4356a6903de7
Damien Buttler <damien@doublehops.com>
2024-08-17 08:54:41 +1000

    Added final endpoint tests to author.

--- internal/handlers/author/author_test.go
+++ internal/handlers/author/author_test.go
@@ -48,16 +48,13 @@ func TestAuthorCRUD(t *testing.T) {
 	assert.NoError(t, err, "unexpected error in request/response")
 	assert.Contains(t, statusCode, fmt.Sprintf("%d", http.StatusOK))
 
-	recordGet := request.SingleItemResp{
-		Data: &author.Author{},
-	}
-	err = json.Unmarshal(res, &recordGet)
+	err = json.Unmarshal(res, &record)
 	assert.NoError(t, err, "unable to unmarshal record")
-	dGet := record.Data.(*author.Author)
+	d = record.Data.(*author.Author)
 
 	assert.NoError(t, err, "error unmarshalling record")
-	assert.Equal(t, payload.Name, dGet.Name)
-	assert.Greater(t, dGet.ID, int32(0))
+	assert.Equal(t, payload.Name, d.Name)
+	assert.Greater(t, d.ID, int32(0))
 	expectedTime, duration = testtools.GetTolerance(5)
 	assert.WithinDuration(t, expectedTime, *d.UpdatedAt, duration)
 
@@ -71,16 +68,26 @@ func TestAuthorCRUD(t *testing.T) {
 	assert.NoError(t, err, "unexpected error in request/response")
 	assert.Contains(t, statusCode, fmt.Sprintf("%d", http.StatusOK))
 
-	recordPut := request.SingleItemResp{
-		Data: &author.Author{},
-	}
-	err = json.Unmarshal(res, &recordPut)
+	err = json.Unmarshal(res, &record)
 	assert.NoError(t, err, "unable to unmarshal record")
-	dPut := recordPut.Data.(*author.Author)
+	d = record.Data.(*author.Author)
 
 	assert.NoError(t, err, "error unmarshalling record")
-	assert.Equal(t, payload.Name, dPut.Name)
-	assert.Greater(t, dPut.ID, int32(0))
+	assert.Equal(t, payload.Name, d.Name)
+	assert.Greater(t, d.ID, int32(0))
 	expectedTime, duration = testtools.GetTolerance(5)
 	assert.WithinDuration(t, expectedTime, *d.UpdatedAt, duration)
+
+	// Test delete new record.
+	path = fmt.Sprintf("v1/author/%d", d.ID)
+	statusCode, res, err = req.MakeRequest(ctx, http.MethodDelete, path, nil, nil)
+	assert.NoError(t, err, "unexpected error in request/response")
+	assert.Contains(t, statusCode, fmt.Sprintf("%d", http.StatusNoContent))
+
+	// Test that record has been deleted.
+	path = fmt.Sprintf("v1/author/%d", d.ID)
+	statusCode, res, err = req.MakeRequest(ctx, http.MethodGet, path, nil, nil)
+	assert.NoError(t, err, "unexpected error in request/response")
+	assert.Contains(t, statusCode, fmt.Sprintf("%d", http.StatusNotFound))
+
 }
