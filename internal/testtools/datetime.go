360ab13b260675305be9fa7352cb2a29d263a4e1
Damien Buttler <damien@doublehops.com>
2024-08-19 10:44:02 +1000

    More linting.

--- internal/testtools/datetime.go
+++ internal/testtools/datetime.go
@@ -5,7 +5,7 @@ import "time"
 func GetTolerance(seconds int) (time.Time, time.Duration) {
 	duration := time.Duration(seconds)
 	timeNow := time.Now()
-	expectedTime := timeNow.Add(-duration * time.Second)
+	expectedTime := timeNow.Add(-duration)
 	tolerance := 10 * time.Second
 
 	return expectedTime, tolerance
