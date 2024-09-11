379c770701401aa582cbb8bebd68586166e0e29c
Damien Buttler <damien@doublehops.com>
2024-09-11 17:29:58 +1000

    Gofmpted.

--- internal/handlers/author/author_test.go
+++ internal/handlers/author/author_test.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/doublehops/dh-go-framework/internal/config"
 	"log"
 	"net/http"
 	"os"
 	"testing"
+
+	"github.com/doublehops/dh-go-framework/internal/config"
 
 	"github.com/stretchr/testify/assert"
 
