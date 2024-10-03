63ef67f2b61d4ab04f3e174f84671bad9df86481
Damien Buttler <damien@doublehops.com>
2024-10-03 17:52:02 +1000

    Corrected email exists error response.

---
+++ internal/service/userservice/userservicefuncs.go
@@ -0,0 +1,23 @@
+package userservice
+
+import (
+	"context"
+	"errors"
+	model "github.com/doublehops/dh-go-framework/internal/model/user"
+)
+
+// EmailAddressAlreadyExists will return bool for existence of email address.
+func (s *UserService) EmailAddressAlreadyExists(ctx context.Context, emailAddress string) (bool, error) {
+	record := &model.User{}
+	if err := s.GetByEmailAddress(ctx, record, emailAddress); err != nil {
+		s.Log.Error(ctx, "error retrieving record by email address"+err.Error(), nil)
+
+		return false, errors.New("error retrieving record by email address")
+	}
+
+	if record.EmailAddress != "" {
+		return true, nil
+	}
+
+	return false, nil
+}
