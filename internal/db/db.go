00e08d8e105a1ecc1756d359e94a550d1fc74a5a
Damien Buttler <damien@doublehops.com>
2026-04-04 14:15:29 +1100

    Added retries to database connection attempt.

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql" // Importing the MySQL driver for database/sql.
 	"github.com/jmoiron/sqlx"
@@ -10,17 +11,30 @@ import (
 	"github.com/doublehops/dh-go-framework/internal/logga"
 )
 
+const (
+	maxAttempts = 5
+	delay       = 5
+)
+
 func New(l *logga.Logga, cfg config.DB) (*sqlx.DB, error) {
 	l.Log.Info("opening database connection")
 
+	var db *sqlx.DB
+	var err error
+
 	connectStr := fmt.Sprintf("%s:%s@(%s:3306)/%s?parseTime=true", cfg.User, cfg.Pass, cfg.Host, cfg.Name)
-	db, err := sqlx.Connect("mysql", connectStr)
-	if err != nil {
+
+	for attempt := 1; attempt <= maxAttempts; attempt++ {
+		db, err = sqlx.Connect("mysql", connectStr)
+		if err == nil {
+			return db, nil
+		}
+
 		l.Log.Error(fmt.Sprintf("unable to open db connection. %s", err))
 		l.Log.Error(fmt.Sprintf("Database connection error. user: %s; password: %s; host: %s; name: %s", cfg.User, cfg.Pass, cfg.Host, cfg.Name))
 
-		return db, err
+		time.Sleep(delay * time.Second)
 	}
 
-	return db, nil
+	return nil, fmt.Errorf("unable to open database connection. max attempts reached. Attempts: %d", maxAttempts)
 }
